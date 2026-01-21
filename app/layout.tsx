bd527af100babf44237f3ac005a4270fce890a4a
DouglasLindahl <douglas.lindahl@gmail.com>
2026-01-20 21:28:45 -0600

    a

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "test",
+  description: "test",
+  openGraph: {
+    title: "test",
+    description: "test",
+    url: "https://futurelogistics.se",
+    siteName: "test",
+    images: [
+      {
+        url: "https://futurelogistics.se/hero_image.JPG",
+        width: 1200,
+        height: 630,
+        alt: "Preview image",
+      },
+    ],
+    type: "website",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
